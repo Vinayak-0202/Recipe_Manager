3a02dff6c77bd92eade08a1487f3139581826832
Vinayak Nikam <vpnikam02@gmail.com>
2023-10-12 23:30:11 +0530

    files are update and database is connected

--- app.js
+++ app.js
@@ -7,8 +7,14 @@ const bodyParser = require("body-parser");
 const app = express();
 
 //DataBase Connect String
-const connString =
-  process.env.CONNECT || "postgres://vinayak:vinayak21@localhost/recipebookdb";
+// const connString =
+//   process.env.CONNECT || "postgres://vinayak:vinayak21@localhost/recipebookdb";
+const config = {
+  user: "postgres",
+  database: "recipebookdb",
+  password: "vinayak21",
+  port: 5433, //Default port, change it if needed
+};
 let port = process.env.PORT || 3000;
 
 // Assign Dust Engine to .dust Files
@@ -25,7 +31,22 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-  res.render("index");
+  //pg connect
+  const pool = new pg.Pool(config);
+  pool.connect(function (err, client, done) {
+    if (err) {
+      return console.error("error fetching clientfrom pool", err);
+    }
+    client.query("SELECT * FROM recipes", function (err, result) {
+      if (err) {
+        return console.error("error running query", err);
+      }
+      done();
+      res.render("index", { recipes: result.rows });
+      // res.status(200).send(result.rows);
+    });
+    pool.end();
+  });
 });
 //server
 app.listen(port, () => console.log(`server is listning on ${port}...`));
